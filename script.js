5a7ff7f811350c347ca2aad1c398c2e2f9e070b0
yellow544 <31159133+yellow544@users.noreply.github.com>
2017-09-24 19:39:13 +0100

    js added'

---
+++ script.js
@@ -0,0 +1,38 @@
+//Utworzenie funkcji konstruującej klasy i dodanie parametrów
+
+function Phone(brand, price, color, screen, storage, system) {
+this.brand = brand;
+this.price = price;
+this.color = color;
+this.screen = screen;
+this.storage = storage;
+this.system = system;
+}
+
+//Utworzenie funkcji prototypowych - opisują metodę za pomocą console.log
+
+Phone.prototype.printInfo = function() {
+	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ', ' + 'the screen size is ' + this.screen + ' inches, ' + 'the storage capacity is ' + this.storage + 'GB and the price is ' + this.price + 'zł.');
+}
+
+Phone.prototype.printSystemInfo = function () {
+	console.log('The ' + this.brand + ' phone is using system ' + this.system + '.');
+
+}
+
+
+
+//Utworzenie obiektów 
+
+const iPhone6s = new Phone('Apple', 2000, 'gold', 4.70, 16, 'iOS');
+const SamsungGalaxyS6 = new Phone('Samsung', 1500, 'white', 5.10, 32, 'Android' );
+const SonyXperiaZ5 = new Phone('Sony', 1200, 'black', 5.20, 32, 'Android');
+
+//Wywołanie metody
+
+iPhone6s.printInfo();
+iPhone6s.printSystemInfo();
+SamsungGalaxyS6.printInfo();
+SamsungGalaxyS6.printSystemInfo();
+SonyXperiaZ5.printInfo();
+SonyXperiaZ5.printSystemInfo();
